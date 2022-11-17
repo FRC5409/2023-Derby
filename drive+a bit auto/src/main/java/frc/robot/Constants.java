3526d4bec8f612e1986ae11234b59f1e43d0df71
Myrica-YW <100385243+Myrica-YW@users.noreply.github.com>
2022-11-17 17:10:56 -0500

    constants

--- drive+a bit auto/src/main/java/frc/robot/Constants.java
+++ drive+a bit auto/src/main/java/frc/robot/Constants.java
@@ -17,11 +17,57 @@ import edu.wpi.first.math.system.plant.LinearSystemId;
  * constants are needed, to reduce verbosity.
  */
 public final class Constants {
+  public static class kDriveTrain {   
+    // Motor CAN IDs
+    public static final int kLeftDriveFront = 14;
+    public static final int kLeftDriveRear = 4;
+    
+    public static final int kRightDriveRear = 6;
+    public static final int kRightDriveFront = 15;
+
+    // Drive mode constants
+    public static final int ARCADE_DRIVE = 1; // Arcade Drive
+    public static final int AADL_DRIVE = 2; // Addl Drive (Arcade drive)
+    public static final int C_DRIVE = 3; // Curvature drive
+    public static final int T_DRIVE = 4; // Tank Drive
+
+    public static final double UP_SHIFT = 300;
+    public static final double DOWN_SHIFT = 260;
+
+    public static final int MOTOR_CURRENT_LIMIT = 60;
+
+
+    public static final double ksVolts = 0.22;
+    public static final double kvVoltSecondsPerMeter = 1.98;
+    public static final double kaVoltSecondsSquaredPerMeter = 0.2;
+
+    // Example value only - as above, this must be tuned for your drive!
+    public static final double PDriveVel = 8.5;
+
+
+    public static final double TrackwidthMeters = 0.69;
+    public static final DifferentialDriveKinematics DriveKinematics = new DifferentialDriveKinematics(
+            TrackwidthMeters);
+
+    public static final double MaxSpeedMetersPerSecond = 3;
+    public static final double MaxAccelerationMetersPerSecondSquared = 3;
+
+    // Reasonable baseline values for a RAMSETE follower in units of meters and
+    // seconds
+    public static final double RamseteB = 2;
+    public static final double RamseteZeta = 0.7;
+    public static final int CANPigeon = 0;
+}
+
+public static class kPneumatics {
+    public static final int PCMId = 0;
+    public static final int MODULE = 0;
+}
   public static final class DriveConstants {
     
     public static final int kLeftMotor1Port = 14; //front
     public static final int kLeftMotor2Port = 4;
-    public static final int kRightMotor1Port = 15;//front
+    public static final int kRightMotor1Port = 15;
     public static final int kRightMotor2Port = 6;
 
     public static final int[] kLeftEncoderPorts = new int[] {0, 1};
