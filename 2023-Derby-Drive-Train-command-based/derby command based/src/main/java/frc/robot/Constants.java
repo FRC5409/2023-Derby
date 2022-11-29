465d0a673859435a7a335d6ae7d5250f03333ec7
Myrica-YW <100385243+Myrica-YW@users.noreply.github.com>
2022-11-29 00:31:24 -0500

    2022.11 command based code update

---
+++ 2023-Derby-Drive-Train-command-based/derby command based/src/main/java/frc/robot/Constants.java
@@ -0,0 +1,58 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot;
+
+/**
+ * The Constants class provides a convenient place for teams to hold robot-wide numerical or boolean
+ * constants. This class should not be used for any other purpose. All constants should be declared
+ * globally (i.e. public static). Do not put anything functional in this class.
+ *
+ * <p>It is advised to statically import this class (or one of its inner classes) wherever the
+ * constants are needed, to reduce verbosity.
+ */
+public final class Constants {
+    public static class kDriveTrain {   
+        // Motor CAN IDs
+        public static final int kLeftDriveFront = 14;
+        public static final int kLeftDriveRear = 4;
+        
+        public static final int kRightDriveRear = 6;
+        public static final int kRightDriveFront = 15;
+
+        // Drive mode constants
+        public static final int ARCADE_DRIVE = 1; // Arcade Drive
+        public static final int AADL_DRIVE = 2; // Addl Drive (Arcade drive)
+        public static final int C_DRIVE = 3; // Curvature drive
+        public static final int T_DRIVE = 4; // Tank Drive
+
+        public static final double UP_SHIFT = 300;
+        public static final double DOWN_SHIFT = 260;
+
+        public static final int MOTOR_CURRENT_LIMIT = 60;
+
+        public static final double ksVolts = 0.22;
+        public static final double kvVoltSecondsPerMeter = 1.98;
+        public static final double kaVoltSecondsSquaredPerMeter = 0.2;
+
+        // Example value only - as above, this must be tuned for your drive!
+        public static final double PDriveVel = 8.5;
+
+        //public static final double TrackwidthMeters = 0.69;
+        //public static final DifferentialDriveKinematics DriveKinematics = new DifferentialDriveKinematics(
+         //       TrackwidthMeters);
+
+        public static final double MaxSpeedMetersPerSecond = 3;
+        public static final double MaxAccelerationMetersPerSecondSquared = 3;
+
+        public static final double RamseteB = 2;
+        public static final double RamseteZeta = 0.7;
+        public static final int CANPigeon = 0;
+    }
+
+    public static class kPneumatics {
+        public static final int PCMId = 0;
+        public static final int MODULE = 0;
+    }
+}
---
+++ 2023-Derby-Drive-Train-command-based/derby command based/src/main/java/frc/robot/commands/Drive.java
@@ -0,0 +1,56 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot.commands;
+
+import frc.robot.subsystems.DriveTrain;
+
+
+import edu.wpi.first.wpilibj.XboxController;
+import edu.wpi.first.wpilibj2.command.CommandBase;
+
+/** An example command that uses an example subsystem. */
+public class Drive extends CommandBase {
+  private final DriveTrain sys_drive;
+  private final XboxController m_joystick;
+
+  /**
+   * Creates a new ExampleCommand.
+   *
+   * @param subsystem The subsystem used by this command.
+   * @param m_subsystem 
+   */
+  public Drive(DriveTrain subsystem, XboxController joystick) {
+    sys_drive = subsystem;
+    m_joystick = joystick;
+    
+    // Use addRequirements() here to declare subsystem dependencies.
+    addRequirements(subsystem);
+  }
+
+  // Called when the command is initially scheduled.
+  @Override
+  public void initialize() {}
+
+  // Called every time the scheduler runs while the command is scheduled.
+  @Override
+  public void execute() {
+    //Left trigger(acceleration), right trigger(deceleration), LeftX
+    double acceleration = m_joystick.getLeftTriggerAxis();
+    double deceleration = m_joystick.getRightTriggerAxis();
+
+    double turnValue = m_joystick.getLeftX();
+
+  }
+
+  // Called once the command ends or is interrupted.
+  @Override
+  public void end(boolean interrupted) {}
+
+  // Returns true when the command should end.
+  @Override
+  public boolean isFinished() {
+    return false;
+  }
+}
