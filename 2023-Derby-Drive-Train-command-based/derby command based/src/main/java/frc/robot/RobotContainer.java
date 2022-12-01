d84abf19a4838af9467edbedacd13134e6dbe717
Myrica-YW <100385243+Myrica-YW@users.noreply.github.com>
2022-12-01 16:33:58 -0500

    drivefwd

--- 2023-Derby-Drive-Train-command-based/derby command based/src/main/java/frc/robot/RobotContainer.java
+++ 2023-Derby-Drive-Train-command-based/derby command based/src/main/java/frc/robot/RobotContainer.java
@@ -6,7 +6,7 @@ package frc.robot;
 
 import edu.wpi.first.wpilibj.GenericHID;
 import edu.wpi.first.wpilibj.XboxController;
-import frc.robot.commands.DeafultDrive;
+import frc.robot.commands.Drivefwd;
 
 import edu.wpi.first.wpilibj2.command.Command;
 
@@ -20,7 +20,7 @@ public class RobotContainer {
   // The robot's subsystems and commands are defined here...
 
   private final XboxController m_joystick_main;
-  private final DeafultDrive sys_DriveTrain;
+  private final Drivefwd sys_DriveTrain;
 
   /** The container for the robot. Contains subsystems, OI devices, and commands. */
   public RobotContainer() {
--- 2023-Derby-Drive-Train-command-based/derby command based/src/main/java/frc/robot/commands/Drivefwd.java
+++ 2023-Derby-Drive-Train-command-based/derby command based/src/main/java/frc/robot/commands/Drivefwd.java
@@ -7,11 +7,41 @@ package frc.robot.commands;
 
 import edu.wpi.first.wpilibj.XboxController;
 import edu.wpi.first.wpilibj2.command.CommandBase;
+import frc.robot.subsystems.DriveTrain;
 
 /** Add your docs here. */
 public class Drivefwd extends CommandBase {
     private float driveSpeed;
     private final DriveTrain sys_drive;
-    private final XboxController m_joystick;
-    
+
+    public Drivefwd(DriveTrain subsystem, XboxController joystick) {
+        sys_drive = subsystem;
+        addRequirements(subsystem);
+    }
+
+
+      // Called when the command is initially scheduled.
+  @Override
+  public void initialize() {}
+
+  // Called every time the scheduler runs while the command is scheduled.
+  @Override
+  public void execute() {
+    //Left trigger(acceleration), right trigger(deceleration), LeftX(turn)
+    sys_drive.tankDrive(driveSpeed, driveSpeed);
+
+  }
+
+  // Called once the command ends or is interrupted.
+  @Override
+  public void end(boolean interrupted) {
+      sys_drive.tankDrive(0, 0);
+  }
+
+  // Returns true when the command should end.
+  @Override
+  public boolean isFinished() {
+    return false;
+  }
 }
+
