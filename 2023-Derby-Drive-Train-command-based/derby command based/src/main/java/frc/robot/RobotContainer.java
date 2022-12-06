0bcf172f0f76af6b21ec207ae0a32d4a90804c6e
Myrica-YW <100385243+Myrica-YW@users.noreply.github.com>
2022-12-06 16:05:25 -0500

    deleted auto

--- 2023-Derby-Drive-Train-command-based/derby command based/src/main/java/frc/robot/RobotContainer.java
+++ 2023-Derby-Drive-Train-command-based/derby command based/src/main/java/frc/robot/RobotContainer.java
@@ -6,10 +6,7 @@ package frc.robot;
 
 import edu.wpi.first.wpilibj.GenericHID;
 import edu.wpi.first.wpilibj.XboxController;
-//import frc.robot.commands.Drivestraight;
-
-
-import edu.wpi.first.wpilibj2.command.Command;
+//import frc.robot.commands.DriveStraight;
 
 /**
  * This class is where the bulk of the robot should be declared. Since Command-based is a
@@ -34,21 +31,25 @@ public class RobotContainer {
     configureButtonBindings();
   }
 
+
+
   /**
    * Use this method to define your button->command mappings. Buttons can be created by
    * instantiating a {@link GenericHID} or one of its subclasses ({@link
    * edu.wpi.first.wpilibj.Joystick} or {@link XboxController}), and then passing it to a {@link
    * edu.wpi.first.wpilibj2.command.button.JoystickButton}.
    */
-  private void configureButtonBindings() {}
+  private void configureButtonBindings() {
+  }
+}
 
   /**
    * Use this to pass the autonomous command to the main {@link Robot} class.
    *
    * @return the command to run in autonomous
    */
-  public Command getAutonomousCommand() {
-    return m_autonomousCommand;
-  }
-}
+//  public Command getAutonomousCommand() {
+//    return m_autonomousCommand;
+//  }
+//}
 
